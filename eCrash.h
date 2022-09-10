b79a16f1cdc41e3b7881796298289ed64eb81f22
Godzil <godzil@godzil.net>
2022-09-11 00:26:06 +0100

    I'm a weirdo, but I needed to do some code reformat and comment cleanups.

--- eCrash.h
+++ eCrash.h
@@ -1,10 +1,9 @@
-/**
+/***
  * \file eCrash.h
  * @author David Frascone
  * 
- *  eCrash types and prototypes.
+ * eCrash types and prototypes.
  *
- * vim: ts=4
  */
 
 #ifndef _ECRASH_H_
@@ -28,31 +27,32 @@ typedef void (*sighandler_t)(int);
 #define ECRASH_DEFAULT_THREAD_WAIT_TIME 10
 #define ECRASH_MAX_NUM_SIGNALS 30
 
-/** \struct eCrashSymbol
- *  \brief Function Name / Address pair
+/***
+ * \struct eCrashSymbol
+ * \brief Function Name / Address pair
  *
- *  This is used in conjunction with eCrashSymbolTable to
- *  provide an alternative to backtrace_symbols.
+ * This is used in conjunction with eCrashSymbolTable to
+ * provide an alternative to backtrace_symbols.
  */
-typedef struct {
-	char          *function;
-	void          *address;
+typedef struct
+{
+    char *function;
+    void *address;
 } eCrashSymbol;
-/** \struct eCrashSymbolTable
- *  \brief Symbol table used to avoid backtrace_symbols()
+
+/***
+ * \struct eCrashSymbolTable
+ * \brief Symbol table used to avoid backtrace_symbols()
  *
- *  This structure is used to provide a alternative to 
- *  backtrace_symbols which is not async signal safe.
+ * This structure is used to provide a alternative to backtrace_symbols which is not async signal safe.
  *
- *  The symbol table should be sorted by address (it will 
- *  be either binary or linearly searched)
+ * The symbol table should be sorted by address (it will be either binary or linearly searched)
  */
-typedef struct {
-	int           numSymbols;
-	eCrashSymbol *symbols;
+typedef struct
+{
+    int numSymbols;
+    eCrashSymbol *symbols;
 } eCrashSymbolTable;
-
-
 
 #define ECRASH_DEBUG_ENABLE  /* undef to turn off debug */
 
@@ -63,105 +63,95 @@ typedef struct {
 # define ECRASH_DEBUG_WARN         4
 # define ECRASH_DEBUG_ERROR        5
 # define ECRASH_DEBUG_OFF          6
-# define ECRASH_DEBUG_DEFAULT	  (ECRASH_DEBUG_ERROR)
-# define DPRINTF(level, fmt...) \
-		if (level >= gbl_params.debugLevel) { printf(fmt); fflush(stdout); }
+# define ECRASH_DEBUG_DEFAULT      (ECRASH_DEBUG_ERROR)
+# define DPRINTF(level, fmt...) if (level >= gbl_params.debugLevel) { printf(fmt); fflush(stdout); }
 #else /* ECRASH_DEBUG_ENABLE */
 # define DPRINTF(level, fmt...) 
 #endif /* ECRASH_DEBUG_ENABLE */
 
-
-/** \struct eCrashParameters
- *  \brief eCrash Initialization Parameters
+/***
+ * \struct eCrashParameters
+ * \brief eCrash Initialization Parameters
  *  
- *  This structure contains all the global initialization functions
- *  for eCrash.
+ * This structure contains all the global initialization functions for eCrash.
  * 
  *  @see eCrash_Init
  */
-typedef struct {
+typedef struct
+{
+    /* OUTPUT OPTIONS */
+    /*** Filename to output to, or NULL */
+    char *filename;
+    /*** FILE * to output to or NULL */
+    FILE *filep;
+    /*** fd to output to or -1 */
+    int fd;
 
+    int debugLevel;
 
-	/*  OUTPUT OPTIONS */
-		/** Filename to output to, or NULL */
-	char *filename;			
-		/** FILE * to output to or NULL */
-	FILE *filep;
-		/** fd to output to or -1 */
-	int	fd;
+    /*** If true, all registered threads will be dumped */
+    BOOL dumpAllThreads;
 
-	int debugLevel;
+    /*** How far to backtrace each stack */
+    unsigned int maxStackDepth;
 
-		/** If true, all registered threads will
-		 *   be dumped
-         */
-	BOOL dumpAllThreads;
+    /*** Default signal to use to tell a thread to drop its stack. */
+    int defaultBacktraceSignal;
 
-		/** How far to backtrace each stack */
-	unsigned int maxStackDepth;
+    /*** How long to wait for a threads dump */
+    unsigned int threadWaitTime;
 
-		/** Default signal to use to tell a thread to drop it's
-		 *  stack.
-		 */
-	int defaultBacktraceSignal;
+    /***
+     * If this is non-zero, the dangerous function, backtrace_symbols will be used. That function does a
+     * malloc(), so is not async signal safe, and could cause deadlocks.
+     */
+    BOOL useBacktraceSymbols;
 
-		/** How long to wait for a threads
-		 *  dump
-		 */	
-	unsigned int threadWaitTime;
+    /***
+     * To avoid the issues with backtrace_symbols (see comments above) the caller can supply it's own symbol
+     * table, containing function names and start addresses. This table can be created using a script, or a
+     * static table.
+     *
+     * If this variable is not null, it will be used, instead of backtrace_symbols, reguardless of the setting
+     * of useBacktraceSymbols.
+     */
+    eCrashSymbolTable *symbolTable;
 
-		/** If this is non-zero, the dangerous function, backtrace_symbols
-		  * will be used.  That function does a malloc(), so is not async
-		  * signal safe, and could cause deadlocks.
-		  */
-	BOOL useBacktraceSymbols; 
-
-		/** To avoid the issues with backtrace_symbols (see comments above)
-		  * the caller can supply it's own symbol table, containing function
-		  * names and start addresses.  This table can be created using 
-		  * a script, or a static table.
-		  *
-		  * If this variable is not null, it will be used, instead of 
-		  * backtrace_symbols, reguardless of the setting
-		  * of useBacktraceSymbols.
-		  */
-	eCrashSymbolTable *symbolTable;
-
-		/** Array of crash signals to catch,
-		 *  ending in 0  I would have left it a [] array, but I
-		 *  do a static copy, so I needed a set size.
-         */
-	int signals[ECRASH_MAX_NUM_SIGNALS];
+    /***
+     * Array of crash signals to catch, ending in 0  I would have left it a [] array, but I do a static
+     * copy, so I needed a set size.
+     */
+    int signals[ECRASH_MAX_NUM_SIGNALS];
 
 } eCrashParameters;
 
-/*!
+/***
  * Initialize eCrash.
  * 
- * This function must be called before calling any other eCrash
- * functions.  It sets up the global behavior of the system.
+ * This function must be called before calling any other eCrash unctions. It sets up the global
+ * behavior of the system.
  *
  * @param params Our input parameters.  The passed in structure will be copied.
  *
  * @return Zero on success.
  */
 int eCrash_Init(eCrashParameters *params);
-/*!
+
+/***
  * UnInitialize eCrash.
  * 
- * This function may be called to de-activate eCrash, release the
- * signal handlers, and free any memory allocated by eCrash.
+ * This function may be called to de-activate eCrash, release the signal handlers, and free any
+ * memory allocated by eCrash.
  *
  * @return Zero on success.
  */
-int eCrash_Uninit( void );
+int eCrash_Uninit(void);
 
-/*!
+/***
  * Register a thread for backtracing on crash.
  * 
- * This function must be called by any thread wanting it's stack
- * dumped in the event of a crash.  The thread my specify what 
- * signal should be used, or the default, SIGUSR1 will be used.
+ * This function must be called by any thread wanting it's stack dumped in the event of a crash.
+ * The thread my specify what signal should be used, or the default, SIGUSR1 will be used.
  *
  * @param name String used to refer to us in crash dumps
  * @param signo Signal to use to generate dump (default: SIGUSR1)
@@ -170,14 +160,13 @@ int eCrash_Uninit( void );
  */
 int eCrash_RegisterThread(char *name, int signo);
 
-/*!
+/***
  * Un-register a thread for stack dumps.
  * 
- * This function may be called to un-register any previously 
- * registered thread.
+ * This function may be called to un-register any previously registered thread.
  *
  * @return Zero on success.
  */
-int eCrash_UnregisterThread( void );
+int eCrash_UnregisterThread(void);
 
 #endif /* _E_CRASH_H_ */
